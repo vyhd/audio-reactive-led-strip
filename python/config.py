1e67a9a69ae6e2dcb0fc7b9e236ff8b34f84f02c
vyhd <165890+vyhd@users.noreply.github.com>
2020-04-29 14:17:26 -0700

    Drop LED count from 60 to 30, as a compromise on precision between the 'bulb' and 'string' sims

--- python/config.py
+++ python/config.py
@@ -48,7 +48,7 @@ USE_GUI = True
 DISPLAY_FPS = True
 """Whether to display the FPS when running (can reduce performance)"""
 
-N_PIXELS = 60
+N_PIXELS = 30
 """Number of pixels in the LED strip (must match ESP8266 firmware)"""
 
 GAMMA_TABLE_PATH = os.path.join(os.path.dirname(__file__), 'gamma_table.npy')
